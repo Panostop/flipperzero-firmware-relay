1fa40f50b8ea4bd7ba26eee4f22af531d0e3e62b
Panostop <94406359+Panostop@users.noreply.github.com>
2026-02-11 12:08:35 +0100

    tweaks

--- relay.py
+++ relay.py
@@ -8,7 +8,8 @@ Created by TESSIER Solal -- 09-feb-2026
 Using the pyscard (smartcard) module, the goal is to simulate a NFC relay attack between
     a legitimate reader and a legitimate access card to grant access to a building
     without the access card being physically close to the reader using
-    an ACR-122 and a FlipperZero with modded firmware by gvinet on GitHub. 
+    an ACR-122 and a FlipperZero with modded firmware by gvinet on GitHub. To run this,
+    you will also need to download and compile the libnfc library.
 I have forked gvinet's repo to add a few upgrades
     (https://github.com/Panostop/flipperzero-firmware-relay). 
 
@@ -51,10 +52,10 @@ CARDTYPE = AnyCardType() #cardtype object for when we will look for the Access C
 
 def getCardInfo():
     """
-    Uses the libnfc library to get UID, ATQA and SAK from the card.
+    Uses the libnfc library to gather UID, ATQA and SAK from the card.
     """
     
-    # Used files instead of pipes for better clarity and because of issues with pipes
+    # Used files for better clarity and because of issues with pipes
     with open("CardInfo.txt", "w") as CardInfo:
         
         # returns the full card info
@@ -68,19 +69,18 @@ def getCardInfo():
         CardInfoLines = [line.rstrip() for line in CardInfo] #load the file in a list
      
         # filter the output to keep the info we need
+        del CardInfoLines[0]
+        del CardInfoLines[0]
+        del CardInfoLines[0]
+        del CardInfoLines[-1]
+        del CardInfoLines[-1]
         
-        del CardInfoLines[0]
-        del CardInfoLines[0]
-        del CardInfoLines[0]
-        del CardInfoLines[-1]
-        del CardInfoLines[-1]
-        print(CardInfoLines)
         # keep only the second half (the actual values after the ': ')
         CardInfoLines = [CardInfoLines[i].split(': ')[1] for i in range(3)]
         print(CardInfoLines)
     
-    #                   ATQA              UID               SAK
-    return CardInfoLines[0], CardInfoLines[1], CardInfoLines[2]
+    # ATQA / UID / SAK
+    return CardInfoLines
     
 
 def main():
@@ -106,29 +106,15 @@ def main():
 
     cardservice.connection.connect()
 
-    ATQA, UID, SAK = getCardInfo()
+    card_info = getCardInfo() # [ATQA, UID, SAK]
     
-    print(ATQA, UID, SAK)
+    print(card_info)
     
-    for i in range(7):
-        # We take the int, 
-        # convert it to hex (OxXX), 
-        # remove the 0x, add a 0 at the beginning (for numbers lower than 0x10), 
-        # keep only the last 2 digits and set them to uppercase.
-
-        # this allows us to have a universal 2-digit hexadecimal format.
-        # It may not be exactly optimized but it's what I can do
-        # (e.g.  4->'0x4' ->'4' ->'04' ->'04'
-        #      160->'0xa0'->'a0'->'0a0'->'A0')
-        UID[i]=("0"+ hex(UID[i])[2:])[-2:].upper()
-    print(f"Found tag !\n\tUID : \
-    {UID[0]}\
-    {UID[1]}\
-    {UID[2]}\
-    {UID[3]}\
-    {UID[4]}\
-    {UID[5]}\
-    {UID[6]}")
+    for i in range(2): #not needed for SAK
+        "".join(card_info[i].split()) #joins all the bytes in a continuous string for later
+    print(card_info)
+        
+    
   
 
 
