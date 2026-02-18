d1b858b712bd02ccee0a52d754c4b600db8fa760
Panostop <94406359+Panostop@users.noreply.github.com>
2026-02-18 15:08:41 +0100

    did not work :(

--- applications/main/nfc/nfc_cli.c
+++ applications/main/nfc/nfc_cli.c
@@ -21,7 +21,7 @@ bool field_on = false;
 bool g_nfc_low_power_mode_off = false;
 bool g_hex_mode = false;
 uint8_t g_uid[20] = {0xB9, 0xCC, 0x13, 0x71, 0x6F, 0x73, 0x81};
-uint8_t g_uid_len = 0x04;
+uint8_t g_uid_len = 0x07;
 uint8_t g_atqa[2] = {0x04, 0x00};
 uint8_t g_sak = 0x20;
 
